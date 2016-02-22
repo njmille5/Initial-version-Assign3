dd33edd7a5256a6acaf0c8700be457fead8e405b
njmille5 <njmille5@asu.edu>
2016-02-22 12:28:03 -0700

    initial version of assignment 3

---
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,60 @@
+package cse360assign3;
+/**
+ * Class to simulate a calculator and record input
+ * @author Nicholas Miller
+ * class pin 606
+ * @version 2/22/2016
+ */
+public class Calculator {
+
+	private int total;
+	/**
+	 * Creates a calculator
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	/**access method
+	 * 
+	 * @return total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	/**add() add value to total
+	 * 
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	/**subtract() subs value from total
+	 * 
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	/**multiply() multiplies total by value
+	 * 
+	 * @param value
+	 */
+	public void multiply (int value) {
+		
+	}
+	/**divide() divides total by value
+	 * 
+	 * @param value
+	 */
+	public void divide (int value) {
+		
+	}
+	/** getHistory() returns all input with a " " between
+	 * each operand and number
+	 * 
+	 * @return input string
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
