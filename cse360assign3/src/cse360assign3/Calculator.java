bf33d8dba486bacdbae8abff9ed1c654c1bb0c34
njmille5 <njmille5@asu.edu>
2016-02-22 13:42:21 -0700

    second version of assignment 3

--- cse360assign3/src/cse360assign3/Calculator.java
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -19,35 +19,38 @@ public class Calculator {
 	 * @return total
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	/**add() add value to total
 	 * 
 	 * @param value
 	 */
 	public void add (int value) {
-		
+		total = total + value;
 	}
 	/**subtract() subs value from total
 	 * 
 	 * @param value
 	 */
 	public void subtract (int value) {
-		
+		total = total - value;
 	}
 	/**multiply() multiplies total by value
 	 * 
 	 * @param value
 	 */
 	public void multiply (int value) {
-		
+		total = total * value;
 	}
 	/**divide() divides total by value
 	 * 
 	 * @param value
 	 */
 	public void divide (int value) {
-		
+		if (value == 0)
+			total = 0;
+		else
+			total = total / value;
 	}
 	/** getHistory() returns all input with a " " between
 	 * each operand and number
--- cse360assign3/src/cse360assign3/CalculatorTest.java
+++ cse360assign3/src/cse360assign3/CalculatorTest.java
@@ -14,27 +14,62 @@ public class CalculatorTest {
 
 	@Test
 	public void testGetTotal() {
-		fail("Not yet implemented");
+		Calculator cTest = new Calculator();
+		assertEquals(cTest.getTotal(), 0);
+		cTest.add(5);
+		assertEquals(cTest.getTotal(), 5);
 	}
 
 	@Test
 	public void testAdd() {
-		fail("Not yet implemented");
+		Calculator cTest = new Calculator();
+		cTest.add(5);
+		assertEquals(cTest.getTotal(), 5);
+		cTest.add(0);
+		assertEquals(cTest.getTotal(), 5);
+		cTest.add(-1);
+		assertEquals(cTest.getTotal(), 4);
 	}
 
 	@Test
 	public void testSubtract() {
-		fail("Not yet implemented");
+		Calculator cTest = new Calculator();
+		cTest.subtract(5);
+		assertEquals(cTest.getTotal(), -5);
+		cTest.subtract(0);
+		assertEquals(cTest.getTotal(), -5);
+		cTest.subtract(-1);
+		assertEquals(cTest.getTotal(), -4);
 	}
 
 	@Test
 	public void testMultiply() {
-		fail("Not yet implemented");
+		Calculator cTest = new Calculator();
+		cTest.multiply(5);
+		assertEquals(cTest.getTotal(), 0);
+		cTest.add(1);
+		cTest.multiply(88);
+		assertEquals(cTest.getTotal(), 88);
+		cTest.multiply(0);
+		assertEquals(cTest.getTotal(), 0);
 	}
 
 	@Test
 	public void testDivide() {
-		fail("Not yet implemented");
+		Calculator cTest = new Calculator();
+		cTest.divide(5);
+		assertEquals(cTest.getTotal(), 0);
+		cTest.add(144);
+		cTest.divide(12);
+		assertEquals(cTest.getTotal(), 12);
+	}
+	
+	@Test
+	public void testDivideByZero() {
+		Calculator cTest = new Calculator();
+		cTest.add(144);
+		cTest.divide(0);
+		assertEquals(cTest.getTotal(), 0);
 	}
 
 	@Test
